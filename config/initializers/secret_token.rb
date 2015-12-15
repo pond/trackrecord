112ba558f49614ee480f127c67e54916a6ecedf2
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2015-12-15 14:36:31 +1300

    More attempts to satisfy Travis

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Trackrecord::Application.config.secret_token = 'see above'
+Trackrecord::Application.config.secret_token = 'Before you can start TrackRecord, set a secret here - generate one with "bundle exec rake secret"'
 
 # When you've set the key above, you can delete or comment out this
 # "raise" line below.
