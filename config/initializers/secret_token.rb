ffc35475b96e26923151e43430019fbd9ecee67d
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2017-08-11 15:22:10 +1200

    Allow env.var to set secret token.
    This avoids having to edit 'secret_token.rb', which is handy during development to avoid causing changes that might accidentally checked in.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,13 +1,17 @@
 # Be sure to restart your server when you modify this file.
-
+#
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Trackrecord::Application.config.secret_token = 'Before you can start TrackRecord, set a secret here - generate one with "bundle exec rake secret"'
+#
+Trackrecord::Application.config.secret_token = ENV[ 'TRACKRECORD_SECRET_TOKEN' ] || '!'
 
-# When you've set the key above, you can delete or comment out this
-# "raise" line below.
 unless Rails.env.test?
-  raise "Before you can start TrackRecord, you must set a key in config/initializers/secret_token.rb and keep it secret!"
+  if Trackrecord::Application.config.secret_token.length < 32
+    raise "Before you can start TrackRecord, you must set a key in " +
+          "config/initializers/secret_token.rb and keep it secret! " +
+          "Set this in environment variable TRACKRECORD_SECRET_TOKEN " +
+          "or, if you prefer, edit the file directly."
+  end
 end
