11fd02bda27a68e119c8ecb84b44bdb1f3142fd5
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2017-08-13 22:06:50 +1200

    Allow test suite to run

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -4,20 +4,5 @@ require 'reports_controller'
 # Re-raise errors caught by the controller.
 class ReportsController; def rescue_action(e) raise e end; end
 
-class ReportsControllerTest < Test::Unit::TestCase
-
-  # #fixtures :data
-  # 
-  # def setup
-  #   @controller = ReportsController.new
-  #   @request    = ActionController::TestRequest.new
-  #   @response   = ActionController::TestResponse.new
-  # end
-  # 
-  # # TODO Replace this with your actual tests
-  # def test_show
-  #   get :show
-  #   assert_response :success
-  #   assert_equal 'image/png', @response.headers['Content-Type']
-  # end
+class ReportsControllerTest < ActiveSupport::TestCase
 end
